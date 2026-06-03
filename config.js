3a78a9353ad7632a84baf153865804f7ec0f5405
wboylan <boylanwalker@gmail.com>
2026-06-03 16:30:47 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.CAREHUB_CONFIG = {
+  DEMO_MODE: false,
+  SUPABASE_URL: "https://zimlkrnqwklakzghlzci.supabase.co",
+  SUPABASE_ANON_KEY: "eyJ...",
+  STRIPE_PRICE_ID: "price_YOUR_MONTHLY_PRICE_ID"
+};
